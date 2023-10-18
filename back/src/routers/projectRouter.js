15d4eef586d5964d7c6e9d6c8c5cc9e910b6ff56
[AI9] 김명지 <kmjoyit21045@gmail.com>
2023-10-18 16:49:54 +0900

    create project 기능 id, author 값 할당 코드 추가함. User의 id(uuid)로 objectId 값을 가져오는 코드를 추가함

--- back/src/routers/projectRouter.js
+++ back/src/routers/projectRouter.js
@@ -2,18 +2,23 @@ import { ProjectModel } from "../db/schemas/project";
 import { User } from "../db/index";
 import { Router } from "express";
 import { login_required } from "../middlewares/login_required";
+// import { Project } from "../db/index";
+import { v4 as uuidv4 } from "uuid";
 
 const projectRouter = Router();
 
 // create project
 projectRouter.post("/", login_required, async (req, res) => {
-  const projects = await ProjectModel.create({
-    ...req.body,
-  });
+  // document 구성
+  const id = uuidv4();
+  const author = await User.findById({ user_id: req.currentUserId });
+  const newProject = { id, author, ...req.body };
+  // document 생성
+  const projects = await ProjectModel.create(newProject);
   res.status(201).json(projects);
 });
 
-// read project
+//read project
 projectRouter.get("/", login_required, async (req, res) => {
   const projects = await ProjectModel.find({}).populate("author");
   res.status(201).json(projects);
@@ -21,21 +26,21 @@ projectRouter.get("/", login_required, async (req, res) => {
 
 // read project by id
 projectRouter.get("/:userId", login_required, async (req, res) => {
-  const userId = req.params.userId;
+  const userId = await User.findById({ user_id: req.params.userId });
   const projects = await ProjectModel.find({ author: userId }).populate(
     "author"
   );
   res.status(201).json(projects);
 });
 
-// read project by userId
-// projectRouter.get("/:userId", login_required, async (req, res) => {
-//   const author = req.params.userId;
-//   const projects = await ProjectModel.find({ author });
+// read project by id()
+// projectRouter.get("/:id", login_required, async (req, res) => {
+//   const id = req.params.id;
+//   const projects = await ProjectModel.find({ id });
 //   res.status(201).json(projects);
 // });
 
-// update project by id
+// update project by id(project document의 ObjectId)
 projectRouter.put("/:id", login_required, async (req, res) => {
   const id = req.params.id;
   const projects = await ProjectModel.updateOne({ id }, { ...req.body });
