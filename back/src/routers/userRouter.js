cdf0b0b35f30c774ef3b9759657597b0a934bc27
[AI9] 김명지 <kmjoyit21045@gmail.com>
2023-10-26 15:58:30 +0900

    Fix: passport res set-cookie 등록되었다. 아직도 deserializeUser 실행이 안된다.

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -44,30 +44,32 @@ userAuthRouter.post(
       if (!user) return next(info);
       return req.login(user, (loginError) => {
         if (loginError) return next(loginError);
-        return next();
+        res.setHeader("set-cookie", `sessionID=${req.sessionID}`);
+        res.status(200).json(user);
+        return;
       });
     })(req, res, next);
-  },
-  async function (req, res, next) {
-    try {
-      const email = req.body.email;
-      const password = req.body.password;
-
-      // 위 데이터를 이용하여 유저 db에서 유저 찾기
-      const user = await userAuthService.getUser({ email, password });
-
-      if (user.errorMessage) {
-        throw new Error(user.errorMessage);
-      }
-
-      res.setHeader("set-cookie", `sesionID=${req.sessionID}`);
-      res.status(200).send(user);
-      console.log(res.getHeaders());
-      next();
-    } catch (error) {
-      next(error);
-    }
   }
+  // async function (req, res, next) {
+  //   try {
+  //     const email = req.body.email;
+  //     const password = req.body.password;
+
+  //     // 위 데이터를 이용하여 유저 db에서 유저 찾기
+  //     const user = await userAuthService.getUser({ email, password });
+
+  //     if (user.errorMessage) {
+  //       throw new Error(user.errorMessage);
+  //     }
+
+  //     res.setHeader("set-cookie", `sesionID=${req.sessionID}`);
+  //     res.status(200).send(user);
+  //     console.log(res.getHeaders());
+  //     next();
+  //   } catch (error) {
+  //     next(error);
+  //   }
+  // }
 );
 
 userAuthRouter.get(
